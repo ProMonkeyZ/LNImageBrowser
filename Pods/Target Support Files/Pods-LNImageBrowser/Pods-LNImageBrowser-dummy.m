51e50941b0e98668e74eb5de62c456598efc676d
zhanglining <zhanglining@syswin.com>
2017-11-01 09:54:46 +0800

    删除多余文件

--- Pods/Target Support Files/Pods-LNImageBrowser/Pods-LNImageBrowser-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LNImageBrowser : NSObject
-@end
-@implementation PodsDummy_Pods_LNImageBrowser
-@end
--- Pods/Target Support Files/Pods-LNImageBrowserUITests/Pods-LNImageBrowserUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LNImageBrowserUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LNImageBrowserUITests
-@end
